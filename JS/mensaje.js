d92f8e07dbc466d9e20596dea6b45ae2ac5dc234
Christian1702 <151297214+Christian1702@users.noreply.github.com>
2024-12-05 10:15:17 -0500

    correción JS 2

--- JS/mensaje.js
+++ JS/mensaje.js
@@ -1,7 +1,7 @@
 const formulario = document.querySelector('form');
 
-  formulario.addEventListener('submit', (event) => {
+formulario.addEventListener('submit', (event) => {
     event.preventDefault();
     alert('Su mensaje se ha enviado con éxito. Gracias por comentar. Esto nos ayuda a mejorar el contenido del blog.');
     formulario.reset();
-  });
+});
